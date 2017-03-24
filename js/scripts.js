b00b158d178c1c09be9f3bc3aca937702f53836d
Starlynne <lewis.start0905@gmail.com>
2017-03-24 10:57:41 -0700

    Add jquery to parse integers, assign value to answer score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-(document).ready(function(){
+$(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault()
 
     var inputOne = parseInt($("#quest1").val());
-    alert(inputOne);
+    var inputTwo = parseInt($("#quest2").val());
+    var inputThree = parseInt($("#quest3").val());
+    var inputFour = parseInt($("#quest4").val());
+    var inputFive = parseInt($("#quest5").val());
+    var inputSix = parseInt($("#quest6").val());
+    var total = (inputOne + inputTwo + inputThree + inputFour + inputFive + inputSix);
+    alert(total); 
   });
 });
