2c84bd95e153cd0f1bf223de4f2fd636b90d5a9d
Starlynne <lewis.start0905@gmail.com>
2017-03-24 12:19:32 -0700

    Add divs, styling for hidden sections that will reveal upon submit, fix div closure issue in main section

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,18 @@ $(document).ready(function(){
     var inputFive = parseInt($("#quest5").val());
     var inputSix = parseInt($("#quest6").val());
     var total = (inputOne + inputTwo + inputThree + inputFour + inputFive + inputSix);
-    alert(total); 
+
+    var result;
+      if (total <= 3){
+        alert("CSS!");
+      } else if (total <= 8){
+        $("#ruby").show();
+      } else if (total <= 14){
+        alert("Java!");
+      } else if (total <= 17){
+        alert("C#!");
+      } else {
+        alert("PHP");
+      }
   });
 });
